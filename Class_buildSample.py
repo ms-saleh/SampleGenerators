d77b2d8904d4f31e25156cb71c42cb4abff60dae
Sadeq Saleh <mss.saleh@gmail.com>
2021-08-06 23:22:37 -0400

    added JSON input files procesing

--- Class_buildSample.py
+++ Class_buildSample.py
@@ -8,8 +8,15 @@ Get the design of experiment for micro channe arrays, build the geometery in
 nTopology Platform and create buildfiles in Nanoscribe
 """
 import os
+import subprocess
+import time
+import json
+import shutil
+from PIL import Image, ImageDraw, ImageFont
 
 class buildSample(object):
+    exePath = r"C:/Program Files/nTopology/nTopology/ntopCL.exe"
+    cleanUp = True
     
     def __init__(self,path):
         self.path = path
@@ -55,6 +62,95 @@ class buildSample(object):
     
     def setCustomBlock(self,nTop):
         self.customBlock=self.path+'\\'+nTop
+        
+    def setMeshMergeBlock(self):
+        self.customBlock=self.path+'\\'+"MeshMerge.ntop"
+        
+    def nTopTemplate(self):
+        # Generate template for MicroChannel nTop
+        Arguments = [self.exePath]               #nTopCL path
+        Arguments.append("-t")              #json template argument
+        Arguments.append(self.customBlock)      #.ntop notebook file path
+        #nTopCL call with arguments
+        print(" ".join(Arguments))
+        output,error = subprocess.Popen(Arguments,stdout = subprocess.PIPE, 
+                   stderr= subprocess.PIPE).communicate()
+        #Print the return messages
+        print(output.decode("utf-8"))
+    
+    def nTopRun(self,jsonFile,nTopFile):
+        # Generate template for MicroChannel nTop
+        Arguments = [self.exePath]                              #nTopCL path
+        Arguments.append("-j")                                  #json input argument
+        Arguments.append(jsonFile)                              #input json file
+        Arguments.append("-o")                                  #output argument
+        Arguments.append(self.path+"\\"+"out.json")             #output json path
+        Arguments.append(nTopFile)                              #.ntop notebook file path
+        #nTopCL call with arguments
+        print("\n".join(Arguments))
+        output,error = subprocess.Popen(Arguments,stdout = subprocess.PIPE, 
+                   stderr= subprocess.PIPE).communicate()
+        #Print the return messages
+        print(output.decode("utf-8"))
+    
+    def createuChannelInputJSON(self):
+        try:
+            with open(self.path+"\\input_template.json") as f:
+                Inputs_JSON = json.load(f)
+        except:
+            self.nTopTemplate()
+            with open(self.path+"\\input_template.json") as f:
+                Inputs_JSON = json.load(f)
+        self.json=[]
+        for index1, Line in enumerate(self.DOE):
+            Dim = Line.strip().split(",")
+            Inputs_JSON['inputs'][0]['value']=self.STLPath+'\\'+'uChannel_'+str(index1)+'.stl'
+            self.json.append(self.path+"\\"+"input_"+str(index1)+".json")
+            for index2, item in enumerate(Inputs_JSON['inputs'][1:]):
+                item['value']=float(Dim[index2])
+            with open(self.json[index1], 'w') as outfile:
+                json.dump(Inputs_JSON, outfile, indent=4)
+     
+    def createMeshMergeInputJSON(self):
+        try:
+            with open(self.path+"\\input_template.json") as f:
+                Inputs_JSON = json.load(f)
+        except:
+            self.nTopTemplate()
+            with open(self.path+"\\input_template.json") as f:
+                Inputs_JSON = json.load(f)
+        for index, item in enumerate(sorted(os.listdir(self.STLPath))):
+            Inputs_JSON['inputs'][4]['value'][index]=self.STLPath +"\\"+item
+        fnt = ImageFont.truetype(r'/Library/Fonts/arial.ttf', 900)
+        img = Image.new('RGB', (1000 , 1000), color = "black")
+        d = ImageDraw.Draw(img)
+        d.text((10,10), self.sampleName[-2:] , font=fnt, fill="blue")
+        img.save(self.path+"\\"+self.sampleName[-2:]+".png")
+        Inputs_JSON['inputs'][0]['value'] = self.STLPath +"\\"+ self.sampleName + "Bottom.stl"
+        Inputs_JSON['inputs'][1]['value'] = self.STLPath +"\\"+ self.sampleName + "uChannel.stl"
+        Inputs_JSON['inputs'][2]['value'] = self.STLPath +"\\"+ self.sampleName + "Top.stl"
+        Inputs_JSON['inputs'][3]['value'] = self.path +"\\"+ self.sampleName[-2:] + ".png"
+        
+        with open(self.path+"\\input.json", 'w') as outfile:
+            json.dump(Inputs_JSON, outfile, indent=4)
+            
+            
+    def createTree(self):
+        if os.path.isdir(self.STLPath):
+            shutil.rmtree(self.STLPath)
+        os.mkdir(self.STLPath)
+        if os.path.isdir(self.buildPath):
+            shutil.rmtree(self.buildPath)
+        os.mkdir(self.buildPath)
+        
+    def createuChannelSTL(self):
+        for JSON in self.json:
+            self.nTopRun(JSON, self.customBlock)
+            if self.cleanUp and os.path.isfile(JSON):
+                os.remove(JSON)
+            
+            
+        
         
         
         
--- MicrochannelArray.py
+++ MicrochannelArray.py
@@ -5,23 +5,30 @@ Created on Fr Mar 12 13:36:25 2021
 @author: Sadeq Saleh @ Cornell.edu
 
 Get the design of experiment for micro channe arrays, build the geometery in 
-nTopology Platform and create buildfiles in Nanoscribe
+"nTopology Platform" and create buildfiles in Nanoscribe
 """
 from env_var import *
 from Class_buildSample import *
-from nTop_template import *
-# import readDOE
-
-
 
 if __name__ == "__main__":
     path=os.getcwd()
     setCredentials(path,'credentials.txt')
     sample = buildSample(path)
+    sample.createTree()
     sample.readDOE()
+    
     sample.readCustomBlock()
-    nTopTemplate(sample.customBlock)
+    sample.nTopTemplate()
+    sample.createuChannelInputJSON()
+    sample.createuChannelSTL()
+    
+    sample.setCustomBlock("MeshMerge.ntop")
+    sample.nTopTemplate()
+    
+
+
     sample.summary()
+    
      
 
 
